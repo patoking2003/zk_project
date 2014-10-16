fe001ebf9421d57bc75de4234d4178abad3178cb
Yurifull <yurifull.evolution@gmail.com>
2014-10-16 00:35:57 -0300

    fdbdfb

--- Yei-dom/src/main/java/com/dom/yei/base/object/entity/MainTest.java
+++ Yei-dom/src/main/java/com/dom/yei/base/object/entity/MainTest.java
@@ -11,7 +11,7 @@ public class MainTest {
 		
 		QYeiUser qYei = new QYeiUser("YeiUser");
 		
-		System.out.println("holads  mundo");
+		System.out.println("holads  mudsfsfndo");
 		System.out.println("hola mundo");
 		
 	}
